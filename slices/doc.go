09d1ae2bb6505bba4a9825890c3ff2693190b504
Sergey Polichnoy <pilatuz@gmail.com>
2023-11-05 12:30:37 +0300

    `ToChannel`, `FromChannel` slice helpers and unit tests

---
+++ slices/doc.go
@@ -0,0 +1,8 @@
+// Package slices contains slice related helpers missing in standard "slices" package.
+//
+// To avoid conflicts with standard "slices" package you can use aliased import:
+//
+//	import (
+//		slices2 "github.com/Pilatuz/conv/slices"
+//	)
+package slices
