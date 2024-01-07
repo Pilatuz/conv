a1668848421ee32f92a9fdae4f23def0f6170b7f
Sergey Polichnoy <pilatuz@gmail.com>
2024-01-07 12:20:31 +0300

    introduce `MustOK` helper

--- must.go
+++ must.go
@@ -1,10 +1,20 @@
 package conv
 
 // Must checks there is no error.
-// Panics otherwise.
+// Panics otherwise!
 func Must[T any](v T, err error) T {
 	if err != nil {
 		panic(err)
+	}
+
+	return v
+}
+
+// MustOK checks condition is OK (true).
+// Panics otherwise!
+func MustOK[T any](v T, ok bool) T {
+	if !ok {
+		panic("not OK")
 	}
 
 	return v
--- must_test.go
+++ must_test.go
@@ -2,12 +2,21 @@ package conv_test
 
 import (
 	"errors"
+	"fmt"
+	"net"
+	"strconv"
 	"testing"
 
 	. "github.com/Pilatuz/conv"
 )
 
-// TestMust unit tests for `Must` function.
+func ExampleMust() {
+	i := Must(strconv.Atoi("123"))
+	fmt.Println(i)
+	// Output: 123
+}
+
+// TestMust unit tests for [Must] function.
 func TestMust(tt *testing.T) {
 	tt.Run("good_str", func(t *testing.T) {
 		fn := func() (string, error) {
@@ -62,6 +71,70 @@ func TestMust(tt *testing.T) {
 		}()
 
 		Must(fn()) // panics!
+		t.Error("no expected panic")
+	})
+}
+
+func ExampleMustOK() {
+	err := &net.ParseError{}
+	w := MustOK(ErrorAs[net.Error](err))
+	fmt.Println(w.Timeout())
+	// Output: false
+}
+
+// TestMustOK unit tests for [MustOK] function.
+func TestMustOK(tt *testing.T) {
+	tt.Run("good_str", func(t *testing.T) {
+		fn := func() (string, bool) {
+			return "foo", true
+		}
+
+		if e, a := "foo", MustOK(fn()); a != e {
+			t.Errorf("expected `%v`, found `%v`", e, a)
+		}
+	})
+
+	tt.Run("good_int", func(t *testing.T) {
+		fn := func() (int, bool) {
+			return 123, true
+		}
+
+		if e, a := 123, MustOK(fn()); a != e {
+			t.Errorf("expected `%v`, found `%v`", e, a)
+		}
+	})
+
+	tt.Run("bad_str", func(t *testing.T) {
+		fn := func() (string, bool) {
+			return "foo", false
+		}
+
+		defer func() {
+			if r := recover(); r != nil {
+				if _, ok := r.(string); !ok {
+					t.Errorf("unexpected panic type `%v`", r)
+				}
+			}
+		}()
+
+		MustOK(fn()) // panics!
+		t.Error("no expected panic")
+	})
+
+	tt.Run("bad_int", func(t *testing.T) {
+		fn := func() (int, bool) {
+			return 123, false
+		}
+
+		defer func() {
+			if r := recover(); r != nil {
+				if _, ok := r.(string); !ok {
+					t.Errorf("unexpected panic type `%v`", r)
+				}
+			}
+		}()
+
+		MustOK(fn()) // panics!
 		t.Error("no expected panic")
 	})
 }
